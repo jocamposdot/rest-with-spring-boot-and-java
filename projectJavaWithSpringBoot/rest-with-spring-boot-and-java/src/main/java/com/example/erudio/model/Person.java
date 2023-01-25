e40ae489eff338856f5928205d12a785dbdd25bf
João Oliveira <joao.oliveira@neurotech.com.br>
2023-01-25 11:43:35 -0300

    Atualizações gerais em Person, PersonServices, e PersonController

--- projectJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/com/example/erudio/model/Person.java
+++ projectJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/com/example/erudio/model/Person.java
@@ -6,20 +6,19 @@ public class Person implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private long id;
+	private Long id;
 	private String firstName;
 	private String lastName;
-	private String addressName;
+	private String address;
 	private String gender;
+	
+	public Person() {}
 
-	public Person() {
-	}
-
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -39,12 +38,12 @@ public class Person implements Serializable {
 		this.lastName = lastName;
 	}
 
-	public String getAddressName() {
-		return addressName;
+	public String getAddress() {
+		return address;
 	}
 
-	public void setAddressName(String addressName) {
-		this.addressName = addressName;
+	public void setAddress(String address) {
+		this.address = address;
 	}
 
 	public String getGender() {
@@ -59,10 +58,10 @@ public class Person implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((addressName == null) ? 0 : addressName.hashCode());
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
 		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
 		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
-		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
 		return result;
 	}
@@ -76,10 +75,10 @@ public class Person implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Person other = (Person) obj;
-		if (addressName == null) {
-			if (other.addressName != null)
+		if (address == null) {
+			if (other.address != null)
 				return false;
-		} else if (!addressName.equals(other.addressName))
+		} else if (!address.equals(other.address))
 			return false;
 		if (firstName == null) {
 			if (other.firstName != null)
@@ -91,7 +90,10 @@ public class Person implements Serializable {
 				return false;
 		} else if (!gender.equals(other.gender))
 			return false;
-		if (id != other.id)
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
 			return false;
 		if (lastName == null) {
 			if (other.lastName != null)
@@ -100,5 +102,4 @@ public class Person implements Serializable {
 			return false;
 		return true;
 	}
-
 }
